a8cf4aecdb53dc3854706410e2d6a5451b744ea3
Noelbiggs28 <noelbiggs28@gmail.com>
2024-02-03 08:06:45 -1000

    made initial structure

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+from states.menu import Menu
+from states.gameplay import Gameplay
+from game import Game
+
+pygame.init()
+
+# set screen dimensions
+screen = pygame.display.set_mode((800, 800))
+
+# create and store all possible screens
+states = {
+    "MENU": Menu(),
+    "GAMEPLAY": Gameplay()
+}
+
+# create game instance and pass in screen, all screeens, startings screen
+game = Game(screen, states, "MENU")
+
+# start game
+game.run()
+
+# quit game
+pygame.quit()
+sys.exit()
---
+++ states/gameplay.py
@@ -0,0 +1,6 @@
+import pygame
+from .basestate import BaseState
+class Gameplay(BaseState):
+    def __init__(self):
+        super(Gameplay, self).__init__()
+  

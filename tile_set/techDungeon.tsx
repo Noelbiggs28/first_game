a380f8227e0ff37ecfc80977bce6a5467db38b60
Noelbiggs28 <noelbiggs28@gmail.com>
2024-02-04 11:11:09 -1000

    fixed tileset

--- tile_set/techDungeon.tsx
+++ tile_set/techDungeon.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="techDungeon" tilewidth="32" tileheight="32" tilecount="7659" columns="111">
- <image source="../../../../Downloads/Tech Dungeon Roguelite - Asset Pack (DEMO)/tileset x3.png" width="3552" height="2208"/>
+ <image source="techDungeon.png" width="3552" height="2208"/>
  <tile id="6">
   <properties>
    <property name="floor" type="int" value="0"/>

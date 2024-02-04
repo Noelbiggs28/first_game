caa54f3978c5dad9504e900dd427da795d2a200d
Noelbiggs28 <noelbiggs28@gmail.com>
2024-02-03 21:21:58 -1000

    added properties to walls and floor

--- tile_set/techDungeon.tsx
+++ tile_set/techDungeon.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="techDungeon" tilewidth="32" tileheight="32" tilecount="7659" columns="111">
  <image source="../../../../Downloads/Tech Dungeon Roguelite - Asset Pack (DEMO)/tileset x3.png" width="3552" height="2208"/>
+ <tile id="133">
+  <properties>
+   <property name="floor" type="int" value="1"/>
+  </properties>
+ </tile>
 </tileset>

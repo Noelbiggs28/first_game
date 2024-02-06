41ef3a46d3ed181c7770441dd508e80470ca86e4
Noelbiggs28 <noelbiggs28@gmail.com>
2024-02-06 13:40:01 -1000

    deleted everything

--- tile_set/town_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="town_tileset" tilewidth="32" tileheight="32" tilecount="45" columns="5">
- <image source="town_tileset.png" width="176" height="288"/>
-</tileset>
